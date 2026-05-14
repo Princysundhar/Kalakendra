68b1433dbc27f9a7730ddc14871ea24323cc733a
Princysundhar <120549576+Princysundhar@users.noreply.github.com>
2026-05-14 21:32:18 +0530

    Update settings.py

--- Kalakendra/settings.py
+++ Kalakendra/settings.py
@@ -80,14 +80,10 @@ WSGI_APPLICATION = 'Kalakendra.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'kalakendra',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD':'root'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
